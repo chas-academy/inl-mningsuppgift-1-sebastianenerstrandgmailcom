a285947686df2a388a82b5155415e39ab578c14e
sebastianenerstrand@gmail.com <sebastianenerstrand@gmail.com>
2025-04-01 06:10:54 +0200

    Dice simulator

--- main.c
+++ main.c
@@ -6,8 +6,24 @@ int main()
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
 
-   
-    // Your code goes here
+    int sum = 0;
+    int dice[100];
+    int amount_of_nums[6] = {0};
+    
+    for (int i = 0; i < 100; i++)
+    {
+        dice[i] = (rand() % 6) + 1;
+        sum += dice[i];
+        amount_of_nums[dice[i]-1]++;
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d\n", amount_of_nums[i]);
+    }
+
+    printf("%d\n", sum);
+    printf("%.1f\n", sum / 100.0);    
 
     return 0;
 }
